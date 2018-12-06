fc5fb45d121d7dad642c21689bd670cf37b2e252
mbindi <m.bindi@flamingosoftware.it>
2018-12-06 16:06:23 +0100

    Commit registration view for administrator

--- PhpProject1/laravel/resources/views/admin/registrations/index.blade.php
+++ PhpProject1/laravel/resources/views/admin/registrations/index.blade.php
@@ -2,94 +2,86 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3 class="page-title">@lang('global.registrations.title')</h3>
-    @can('registration_create')
-    <p>
-        <a href="{{ route('admin.registrations.create') }}" class="btn btn-success">@lang('global.app_add_new')</a>
-        
-    </p>
-    @endcan
-
-    
-
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            @lang('global.app_list')
-        </div>
-
-        <div class="panel-body table-responsive">
-            <table class="table table-bordered table-striped {{ count($registrations) > 0 ? 'datatable' : '' }} @can('registration_delete') dt-select @endcan">
-                <thead>
-                    <tr>
-                        @can('registration_delete')
-                            <th style="text-align:center;"><input type="checkbox" id="select-all" /></th>
-                        @endcan
-
-                        <th>@lang('global.registrations.fields.nome-documento')</th>
-                        <th>@lang('global.registrations.fields.luogo-rilascio')</th>
-                        <th>@lang('global.registrations.fields.data-emissione')</th>
-                        <th>@lang('global.registrations.fields.data-scadenza')</th>
-                        <th>@lang('global.registrations.fields.id-tipo-doc')</th>
-                        <th>@lang('global.registrations.fields.path-img-doc')</th>
-                        <th>@lang('global.registrations.fields.note')</th>
-                        <th>@lang('global.registrations.fields.registrationscol')</th>
-                                                <th>&nbsp;</th>
-
-                    </tr>
-                </thead>
-                
-                <tbody>
-                    @if (count($registrations) > 0)
-                        @foreach ($registrations as $registration)
-                            <tr data-entry-id="{{ $registration->id }}">
-                                @can('registration_delete')
-                                    <td></td>
-                                @endcan
-
-                                <td field-key='nome_documento'>{{ $registration->nome_documento }}</td>
-                                <td field-key='luogo_rilascio'>{{ $registration->luogo_rilascio }}</td>
-                                <td field-key='data_emissione'>{{ $registration->data_emissione }}</td>
-                                <td field-key='data_scadenza'>{{ $registration->data_scadenza }}</td>
-                                <td field-key='id_tipo_doc'>{{ $registration->id_tipo_doc }}</td>
-                                <td field-key='path_img_doc'>{{ $registration->path_img_doc }}</td>
-                                <td field-key='note'>{{ $registration->note }}</td>
-                                <td field-key='registrationscol'>{{ $registration->registrationscol }}</td>
-                                                                <td>
-                                    @can('registration_view')
-                                    <a href="{{ route('admin.registrations.show',[$registration->id]) }}" class="btn btn-xs btn-primary">@lang('global.app_view')</a>
-                                    @endcan
-                                    @can('registration_edit')
-                                    <a href="{{ route('admin.registrations.edit',[$registration->id]) }}" class="btn btn-xs btn-info">@lang('global.app_edit')</a>
-                                    @endcan
-                                    @can('registration_delete')
-{!! Form::open(array(
-                                        'style' => 'display: inline-block;',
-                                        'method' => 'DELETE',
-                                        'onsubmit' => "return confirm('".trans("global.app_are_you_sure")."');",
-                                        'route' => ['admin.registrations.destroy', $registration->id])) !!}
-                                    {!! Form::submit(trans('global.app_delete'), array('class' => 'btn btn-xs btn-danger')) !!}
-                                    {!! Form::close() !!}
-                                    @endcan
-                                </td>
-
-                            </tr>
-                        @endforeach
-                    @else
-                        <tr>
-                            <td colspan="19">@lang('global.app_no_entries_in_table')</td>
-                        </tr>
-                    @endif
-                </tbody>
-            </table>
-        </div>
+<div class="mdl-card">
+    <div class="mdl-card__title">
+        <h2 class="mdl-card__title-text">@lang('global.registrations.title')</h2>
     </div>
+
+    <div class="mdl-card__supporting-text">
+
+        <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp {{ count($registrations) > 0 ? 'datatable' : '' }} @can('registration_delete') dt-select @endcan">
+            <thead>
+                <tr>
+                    @can('registration_delete')
+                    <th style="text-align:center;"><input type="checkbox" id="select-all" /></th>
+                    @endcan
+
+                    <th class="mdl-data-table__cell--non-numeric">@lang('global.registrations.fields.nome-documento')</th>
+                    <th class="mdl-data-table__cell--non-numeric">@lang('global.registrations.fields.luogo-rilascio')</th>
+                    <th class="mdl-data-table__cell--non-numeric">@lang('global.registrations.fields.data-emissione')</th>
+                    <th class="mdl-data-table__cell--non-numeric">@lang('global.registrations.fields.data-scadenza')</th>
+                    <th class="mdl-data-table__cell--non-numeric">@lang('global.registrations.fields.id-tipo-doc')</th>
+                    <th class="mdl-data-table__cell--non-numeric">@lang('global.registrations.fields.path-img-doc')</th>
+                    <th class="mdl-data-table__cell--non-numeric">&nbsp;</th>
+
+                </tr>
+            </thead>
+
+            <tbody>
+                @if (count($registrations) > 0)
+                @foreach ($registrations as $registration)
+                <tr data-entry-id="{{ $registration->id }}">
+                    @can('registration_delete')
+                    <td></td>
+                    @endcan
+
+                    <td field-key='nome_documento'>{{ $registration->nome_documento }}</td>
+                    <td field-key='luogo_rilascio'>{{ $registration->luogo_rilascio }}</td>
+                    <td field-key='data_emissione'>{{ $registration->data_emissione }}</td>
+                    <td field-key='data_scadenza'>{{ $registration->data_scadenza }}</td>
+                    <td field-key='id_tipo_doc'>{{ $registration->id_tipo_doc }}</td>
+                    <td field-key='path_img_doc'>{{ $registration->path_img_doc }}</td>
+                    <td>
+                        @can('registration_view')
+                        <a href="{{ route('admin.registrations.show',[$registration->id]) }}" class="btn btn-xs btn-primary">@lang('global.app_view')</a>
+                        @endcan
+                        @can('registration_edit')
+                        <a href="{{ route('admin.registrations.edit',[$registration->id]) }}" class="btn btn-xs btn-info">@lang('global.app_edit')</a>
+                        @endcan
+                        @can('registration_delete')
+                        {!! Form::open(array(
+                        'style' => 'display: inline-block;',
+                        'method' => 'DELETE',
+                        'onsubmit' => "return confirm('".trans("global.app_are_you_sure")."');",
+                        'route' => ['admin.registrations.destroy', $registration->id])) !!}
+                        {!! Form::submit(trans('global.app_delete'), array('class' => 'btn btn-xs btn-danger')) !!}
+                        {!! Form::close() !!}
+                        @endcan
+                    </td>
+
+                </tr>
+                @endforeach
+                @else
+                <tr>
+                    <td colspan="19">@lang('global.app_no_entries_in_table')</td>
+                </tr>
+                @endif
+            </tbody>
+        </table>
+    </div>
+    <div class="mdl-card__actions">
+        @can('registration_create')
+        <a href="{{ route('admin.registrations.create') }}" class="btn btn-success">@lang('global.app_add_new')</a>
+        @endcan
+    </div>
+</div>
 @stop
 
 @section('javascript') 
-    <script>
-        @can('registration_delete')
+<script>
+    @can('registration_delete')
             window.route_mass_crud_entries_destroy = '{{ route('admin.registrations.mass_destroy') }}';
-        @endcan
+    @endcan
 
-    </script>
+</script>
 @endsection
--- PhpProject1/laravel/resources/views/admin/registrations/show.blade.php
+++ PhpProject1/laravel/resources/views/admin/registrations/show.blade.php
@@ -1,77 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3 class="page-title">@lang('global.registrations.title')</h3>
 
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            @lang('global.app_view')
-        </div>
 
-        <div class="panel-body table-responsive">
-            <div class="row">
-                <div class="col-md-6">
-                    <table class="table table-bordered table-striped">
-                        <tr>
-                            <th>@lang('global.registrations.fields.nome-documento')</th>
-                            <td field-key='nome_documento'>{{ $registration->nome_documento }}</td>
-                        </tr>
-                        <tr>
-                            <th>@lang('global.registrations.fields.luogo-rilascio')</th>
-                            <td field-key='luogo_rilascio'>{{ $registration->luogo_rilascio }}</td>
-                        </tr>
-                        <tr>
-                            <th>@lang('global.registrations.fields.data-emissione')</th>
-                            <td field-key='data_emissione'>{{ $registration->data_emissione }}</td>
-                        </tr>
-                        <tr>
-                            <th>@lang('global.registrations.fields.data-scadenza')</th>
-                            <td field-key='data_scadenza'>{{ $registration->data_scadenza }}</td>
-                        </tr>
-                        <tr>
-                            <th>@lang('global.registrations.fields.id-tipo-doc')</th>
-                            <td field-key='id_tipo_doc'>{{ $registration->id_tipo_doc }}</td>
-                        </tr>
-                        <tr>
-                            <th>@lang('global.registrations.fields.path-img-doc')</th>
-                            <td field-key='path_img_doc'>{{ $registration->path_img_doc }}</td>
-                        </tr>
-                        <tr>
-                            <th>@lang('global.registrations.fields.note')</th>
-                            <td field-key='note'>{{ $registration->note }}</td>
-                        </tr>
-                        <tr>
-                            <th>@lang('global.registrations.fields.registrationscol')</th>
-                            <td field-key='registrationscol'>{{ $registration->registrationscol }}</td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
+<div class="mdl-card">
+    <div class="mdl-card__title mdl-card--border">
+        <h2 class="mdl-card__title-text">@lang('global.registrations.title')</h2>
+    </div>
 
-            <p>&nbsp;</p>
+    <div class="mdl-card__supporting-text">
 
-            <a href="{{ route('admin.registrations.index') }}" class="btn btn-default">@lang('global.app_back_to_list')</a>
+        <table class="table table-bordered table-striped">
+            <tr>
+                <th>@lang('global.registrations.fields.nome-documento')</th>
+                <th>@lang('global.registrations.fields.luogo-rilascio')</th>
+                <th>@lang('global.registrations.fields.data-emissione')</th>
+                <th>@lang('global.registrations.fields.data-scadenza')</th>
+                <th>@lang('global.registrations.fields.id-tipo-doc')</th>
+                <th>@lang('global.registrations.fields.path-img-doc')</th>
+            </tr>
+            <tr>
+                <td field-key='nome_documento'>{{ $registration->nome_documento }}</td>
+                <td field-key='luogo_rilascio'>{{ $registration->luogo_rilascio }}</td>            
+                <td field-key='data_emissione'>{{ $registration->data_emissione }}</td>   
+                <td field-key='data_scadenza'>{{ $registration->data_scadenza }}</td>
+                <td field-key='id_tipo_doc'>{{ $registration->id_tipo_doc }}</td>
+                <td field-key='path_img_doc'>{{ $registration->path_img_doc }}</td>  
+            </tr>
+            
+        </table>
+        <div>
+            {{ $registration->note }}
         </div>
     </div>
+    <div class="mdl-card__actions">
+        <a href="{{ route('admin.registrations.index') }}" class="btn btn-default">@lang('global.app_back_to_list')</a>
+    </div>
+</div>
 @stop
 
 @section('javascript')
-    @parent
+@parent
 
-    <script src="{{ url('adminlte/plugins/datetimepicker/moment-with-locales.min.js') }}"></script>
-    <script src="{{ url('adminlte/plugins/datetimepicker/bootstrap-datetimepicker.min.js') }}"></script>
-    <script>
-        $(function(){
-            moment.updateLocale('{{ App::getLocale() }}', {
-                week: { dow: 1 } // Monday is the first day of the week
-            });
-            
-            $('.date').datetimepicker({
-                format: "{{ config('app.date_format_moment') }}",
-                locale: "{{ App::getLocale() }}",
-            });
-            
-        });
-    </script>
-            
+<script src="{{ url('adminlte/plugins/datetimepicker/moment-with-locales.min.js') }}"></script>
+<script src="{{ url('adminlte/plugins/datetimepicker/bootstrap-datetimepicker.min.js') }}"></script>
+<script>
+$(function () {
+moment.updateLocale('{{ App::getLocale() }}', {
+    week: {dow: 1} // Monday is the first day of the week
+});
+
+$('.date').datetimepicker({
+    format: "{{ config('app.date_format_moment') }}",
+    locale: "{{ App::getLocale() }}",
+});
+
+});
+</script>
+
 @stop
